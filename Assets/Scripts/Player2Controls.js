9d5ab1c83426f9d49884f51496835d65b91db7e0
5ello <khalil.m.kh@hotmail.com>
2017-04-22 18:14:01 +0400

    Created a second character, created a script to allow keyboard movement, imported skeleton package

---
+++ Assets/Scripts/Player2Controls.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+
+	if(Input.GetKey(KeyCode.I))
+	{
+		transform.Translate(Vector3.forward * (Time.deltaTime * 2.0));
+		GetComponent.<Animation>().Play("run");
+	}
+	else
+		GetComponent.<Animation>().Play("idle");
+
+	if(Input.GetKey(KeyCode.K))
+		transform.Translate(Vector3.back * Time.deltaTime);
+
+	if(Input.GetKey(KeyCode.L))
+		transform.Rotate(Vector3.up);
+
+	if(Input.GetKey(KeyCode.J))
+		transform.Rotate(Vector3.down);
+
+	if(Input.GetKey(KeyCode.J))
+		transform.Rotate(Vector3.down);
+}

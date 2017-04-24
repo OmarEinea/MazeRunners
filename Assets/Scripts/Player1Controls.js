18473b4d1ba0309b68d2739119de160886a85090
5ello <khalil.m.kh@hotmail.com>
2017-04-24 23:38:36 +0400

    Created Desert map, imported new assets, added walls, added decorations, added new zombie character with controls (w,a,s,d,r), added new prefabs

---
+++ Assets/Scripts/Player1Controls.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+
+	private var controller : CharacterController;
+	private var gravity : float = 17.0f;
+	private var jumpPower : float = 6.0f;
+	private var verticalVelocity : float;
+
+function Start () {
+		controller = GetComponent(CharacterController);
+}
+
+function Update () {
+
+	if(Input.GetKey(KeyCode.W))
+	{
+		transform.Translate(Vector3(0, 0, 0.089));// (Time.deltaTime * 2.0));
+		GetComponent.<Animation>().Play("run");
+	}
+
+	else if(Input.GetKey(KeyCode.S)){
+		transform.Translate(Vector3(0, 0, -0.07));
+		GetComponent.<Animation>().Play("run");
+	}
+	else if(Input.GetKeyDown(KeyCode.E))
+		GetComponent.<Animation>().Play("attack");
+
+	else
+		GetComponent.<Animation>().Play("waitingforbattle");
+
+	if(Input.GetKey(KeyCode.D))
+		transform.Rotate(Vector3.up * 2);
+
+	if(Input.GetKey(KeyCode.A))
+		transform.Rotate(Vector3.down * 2);
+
+//Jumping
+	if (controller.isGrounded) {
+		verticalVelocity = -gravity * Time.deltaTime;
+		if (Input.GetKey (KeyCode.R)) {
+			verticalVelocity = jumpPower;
+		}
+	} 
+	else {
+		verticalVelocity -= gravity * Time.deltaTime;
+	}
+
+	controller.Move((Vector3 (0, verticalVelocity, 0)*Time.deltaTime));
+
+}
--- Assets/Scripts/Player2Controls.js
+++ Assets/Scripts/Player2Controls.js
@@ -1,22 +1,31 @@
 ﻿#pragma strict
 
+	private var controller : CharacterController;
+	private var gravity : float = 17.0f;
+	private var jumpPower : float = 6.0f;
+	private var verticalVelocity : float;
+
 function Start () {
-	
+		controller = GetComponent(CharacterController);
 }
 
 function Update () {
 
 	if(Input.GetKey(KeyCode.I))
 	{
-		transform.Translate(Vector3.forward * (Time.deltaTime * 2.0));
+		transform.Translate(Vector3(0, 0, 0.089));// (Time.deltaTime * 2.0));
 		GetComponent.<Animation>().Play("run");
 	}
+
+	else if(Input.GetKey(KeyCode.K)){
+		transform.Translate(Vector3(0, 0, -0.07));
+		GetComponent.<Animation>().Play("run");
+	}
+	else if(Input.GetKeyDown(KeyCode.O))
+		GetComponent.<Animation>().Play("attack");
+
 	else
 		GetComponent.<Animation>().Play("waitingforbattle");
-
-	if(Input.GetKey(KeyCode.K)){
-		transform.Translate(Vector3.back * Time.deltaTime);
-	}
 
 	if(Input.GetKey(KeyCode.L))
 		transform.Rotate(Vector3.up * 2);
@@ -24,5 +33,17 @@ function Update () {
 	if(Input.GetKey(KeyCode.J))
 		transform.Rotate(Vector3.down * 2);
 
+//Jumping
+	if (controller.isGrounded) {
+		verticalVelocity = -gravity * Time.deltaTime;
+		if (Input.GetKey (KeyCode.P)) {
+			verticalVelocity = jumpPower;
+		}
+	} 
+	else {
+		verticalVelocity -= gravity * Time.deltaTime;
+	}
+
+	controller.Move((Vector3 (0, verticalVelocity, 0)*Time.deltaTime));
 
 }

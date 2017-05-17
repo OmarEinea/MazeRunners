2f1f02e9a9d3589c4c39baa15a1a9c9728ebafcc
Omar Einea <eineao@gmail.com>
2017-05-17 18:26:49 +0400

    Add New Character and Weapons and Their Logic

--- Assets/Scripts/rbPlayerControls.js
+++ Assets/Scripts/rbPlayerControls.js
@@ -1,10 +1,6 @@
 ﻿#pragma strict
 
 private var rb:Rigidbody;
-var myLight:Light;
-
-var myLight1:Light;
-var myLight2:Light;
 
 var up:String;
 var down:String;
@@ -12,54 +8,40 @@ var left:String;
 var right:String;
 var jump:String;
 var attack:String;
+
+var weapons:Transform;
+
 private var speed:int = 5;
-private var sendMsg:int;
+private var weaponPicked = false;
+private var anim:Animation;
 
 function Start () {
 	rb = GetComponent.<Rigidbody>();
+	anim = GetComponent.<Animation>();
 }
 
 function OnCollisionEnter(col:Collision){
 
-	if(col.gameObject.tag == "Pyramid"){
-
-		up = "x";
-		down = "x";
-		left = "x";
-		right = "x";
-		jump = "x";
-		attack = "x";
-
-		sendMsg = 1;
+	if(col.gameObject.tag == "PickWeapon" && !weaponPicked) {
+		weapons.Find(col.gameObject.name).gameObject.SetActive(true);
+		weaponPicked = true;
 	}
-
 }
 
 function Update () {
 	var oldY:float = rb.velocity.y;
 	if(Input.GetKey(up)){
 		rb.velocity = speed * transform.forward;
-		GetComponent.<Animation>().Play("run");
+		anim.Play("run");
 	}
 	else if(Input.GetKey(down)){
 		rb.velocity = (1-speed) * transform.forward;
-		GetComponent.<Animation>().Play("run");
+		anim.Play("run");
 	}
-	else if(Input.GetKeyDown(attack))
-		GetComponent.<Animation>().Play("attack");
-
-	else if(sendMsg == 1){
-		GetComponent.<Animation>().Play("dance");
-		if(myLight.enabled != false){
-			myLight.enabled = false;
-			myLight1.enabled = true;
-			myLight2.enabled = true;
-
-		}
-	}
-
-	else if(sendMsg == 0)
-		GetComponent.<Animation>().Play("waitingforbattle");
+	else if(Input.GetKeyDown(attack) && weaponPicked)
+		anim.Play("attack");
+	else if(!anim.IsPlaying("attack"))
+		anim.Play("idle");
 
 	if(Input.GetKey(right))
 		transform.Rotate(Vector3.up * speed/2);
@@ -69,7 +51,6 @@ function Update () {
 
 	if(Input.GetKeyDown(jump))
 		rb.AddForce(Vector3.up * 300);
-
 
 
 	rb.velocity = Vector3(rb.velocity.x, oldY, rb.velocity.z);
